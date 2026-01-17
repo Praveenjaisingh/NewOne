732b3644f288cd8fd6bfd90071d6d1d1dc716e65
PRAVEEN J <144309087+Praveenjaisingh@users.noreply.github.com>
2026-01-17 10:03:23 +0530

    Update mail.service.js

--- portfolio/src/services/mail.service.js
+++ portfolio/src/services/mail.service.js
@@ -2,29 +2,32 @@ const fs = require("fs");
 const path = require("path");
 const { sendMail } = require("../helpers/mailer");
 
-
 class MailService {
-
     async sendContactMail(data) {
+        const { name, email, message } = data;
+
+        if (!name || !email || !message) {
+            throw new Error("Invalid email payload");
+        }
+
         let template = fs.readFileSync(
             path.join(__dirname, "../template/contact.html"),
             "utf8"
         );
 
         template = template
-            .replace(/{{name}}/g, data.name)
-            .replace(/{{email}}/g, data.email)
-            .replace(/{{message}}/g, data.message);
+            .replace(/{{name}}/g, name)
+            .replace(/{{email}}/g, email)
+            .replace(/{{message}}/g, message);
 
         await sendMail({
             to: process.env.ADMIN_EMAIL, 
             subject: "New Portfolio Contact Message",
-            html: template
+            html: template,
         });
 
         return true;
     }
-
 }
 
 module.exports = new MailService();
